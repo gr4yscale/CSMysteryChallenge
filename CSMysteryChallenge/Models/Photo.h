fb1ef2063cdafa5e554a78b56b25e1c7a6882f6f
Tyler Powers <gr4yscale@gmail.com>
2014-08-10 01:47:31 -0700

    add core data model and photo entity subclass

---
+++ CSMysteryChallenge/Models/Photo.h
@@ -0,0 +1,25 @@
+//
+//  Photo.h
+//  CSMysteryChallenge
+//
+//  Created by Tyler Powers on 8/10/14.
+//  Copyright (c) 2014 Couchsurfing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Photo : NSManagedObject
+
+@property (nonatomic, retain) NSString * caption;
+@property (nonatomic, retain) NSNumber * identifier;
+@property (nonatomic, retain) NSString * imagePermalink;
+@property (nonatomic, retain) NSNumber * noteCount;
+@property (nonatomic, retain) id photos;
+@property (nonatomic, retain) NSString * postURL;
+@property (nonatomic, retain) NSString * shortURL;
+@property (nonatomic, retain) id tags;
+@property (nonatomic, retain) NSDate * timestamp;
+
+@end
---
+++ CSMysteryChallenge/Models/Photo.m
@@ -0,0 +1,24 @@
+//
+//  Photo.m
+//  CSMysteryChallenge
+//
+//  Created by Tyler Powers on 8/10/14.
+//  Copyright (c) 2014 Couchsurfing. All rights reserved.
+//
+
+#import "Photo.h"
+
+
+@implementation Photo
+
+@dynamic caption;
+@dynamic identifier;
+@dynamic imagePermalink;
+@dynamic noteCount;
+@dynamic photos;
+@dynamic postURL;
+@dynamic shortURL;
+@dynamic tags;
+@dynamic timestamp;
+
+@end
