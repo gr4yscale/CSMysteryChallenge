8f34c7337f317dc4429bb86e2d308678e70d1c0a
Tyler Powers <gr4yscale@gmail.com>
2014-08-10 04:20:51 -0700

    build managed object contexts for shared data access object

--- CSMysteryChallenge/Models/CSDataAccess.m
+++ CSMysteryChallenge/Models/CSDataAccess.m
@@ -12,10 +12,35 @@
 @interface CSDataAccess ()
 
 @property (nonatomic, strong) id<CSPersisting> store;
+@property (nonatomic, strong) NSManagedObjectContext *persistentStoreMOC;
+@property (nonatomic, strong) NSManagedObjectContext *mainQueueMOC;
 
 @end
 
 
 @implementation CSDataAccess
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        [self buildManagedObjectContexts];
+    }
+    return self;
+}
+
+
+#pragma mark - Private
+
+- (void)buildManagedObjectContexts {
+    self.persistentStoreMOC = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSPrivateQueueConcurrencyType];
+    self.persistentStoreMOC.persistentStoreCoordinator = self.store.persistentStoreCoordinator;
+    self.persistentStoreMOC.mergePolicy = NSMergeByPropertyStoreTrumpMergePolicy;
+    
+    // Create an MOC for use on the main queue
+    self.mainQueueMOC = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSMainQueueConcurrencyType];
+    self.mainQueueMOC.parentContext = self.persistentStoreMOC;
+    self.mainQueueMOC.mergePolicy = NSMergeByPropertyStoreTrumpMergePolicy;
+}
 
 @end
