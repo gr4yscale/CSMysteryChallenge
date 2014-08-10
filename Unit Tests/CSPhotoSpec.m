124fda25489214cb1804928a46c61271204cfc76
Tyler Powers <gr4yscale@gmail.com>
2014-08-10 02:11:40 -0700

    skeleton CSPhotoSpec...time for helpers

---
+++ Unit Tests/CSPhotoSpec.m
@@ -0,0 +1,18 @@
+//
+//  CSPhotoSpec.m
+//  CSMysteryChallenge
+//
+//  Created by Tyler Powers on 8/10/14.
+//  Copyright 2014 Couchsurfing. All rights reserved.
+//
+
+#import <Kiwi/Kiwi.h>
+#import "CSPhoto.h"
+
+SPEC_BEGIN(CSPhotoSpec)
+
+describe(@"CSPhoto", ^{
+  
+});
+
+SPEC_END
