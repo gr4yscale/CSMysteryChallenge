647ec0ad91feb45e0b8b43ad01663f24e4819eae
Tyler Powers <gr4yscale@gmail.com>
2014-08-10 19:28:08 -0700

    add mappings and tests for attributes that don't require a custom NSValueTransformer

--- Unit Tests/CSPostSpec.m
+++ Unit Tests/CSPostSpec.m
@@ -36,8 +36,38 @@ describe(@"CSPost", ^{
     });
     
     describe(@"entity mapping", ^{
-        it(@"mapps identifier", ^{
+        it(@"maps identifier", ^{
             [[post.identifier should] equal:@(94094321065)];
+        });
+        it(@"maps caption", ^{
+            [[post.caption should] containString:@"I am in love with the mystery that is traveling"];
+        });
+        it(@"maps imagePermalink", ^{
+            [[post.imagePermalink should] equal:@"http://couchsurfing.tumblr.com/image/94094321065"];
+        });
+        it(@"maps noteCount", ^{
+            [[post.noteCount should] equal:@(13)];
+        });
+        it(@"maps photos", ^{
+            [[[post.photos firstObject][@"original_size"][@"url"] should] equal:@"http://38.media.tumblr.com/87641fcc29ed76423648688b24c2abca/tumblr_n9yfeuezgw1s50vogo1_1280.jpg"];
+        });
+        it(@"maps postURL", ^{
+            [[post.postURL should] equal:@"http://couchsurfing.tumblr.com/post/94094321065/i-am-in-love-with-the-mystery-that-is-traveling"];
+        });
+        it(@"maps shortURL", ^{
+            [[post.shortURL should] equal:@"http://tmblr.co/Zp5F6t1NeTMMf"];
+        });
+        it(@"maps tags", ^{
+            [[post.tags should] contain:@"couchsurfing"];
+            [[post.tags should] contain:@"road trip"];
+            [[post.tags should] contain:@"adventures"];
+            [[post.tags should] contain:@"travel"];
+            [[post.tags should] contain:@"Travel Photography"];
+            [[post.tags should] contain:@"roadtrip"];
+            [[post.tags should] contain:@"western united states"];
+        });
+        pending(@"maps timestamp", ^{
+            
         });
     });
   
